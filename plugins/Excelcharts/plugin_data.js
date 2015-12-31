bad290c1f4ea3a20d93a7c7f0c61fbd3b1052660
zenithtekla <zenithtekla@gmail.com>
2015-12-31 17:09:28 +0000

    r6.1 new branch for Excelchart plugin

---
+++ plugins/Excelcharts/plugin_data.js
@@ -0,0 +1,198 @@
+var plugin_data = {
+    "customer": [
+        {
+            "customer_id": "01",
+            "customer_name": "Qualcomm"
+        },
+        {
+            "customer_id": "02",
+            "customer_name": "Telnet"
+        },
+        {
+            "customer_id": "03",
+            "customer_name": "Mantra"
+        },
+        {
+            "customer_id": "04",
+            "customer_name": "Tenet"
+        },
+        {
+            "customer_id": "05",
+            "customer_name": "Quaridge"
+        },
+        {
+            "customer_id": "06",
+            "customer_name": "Antrope"
+        }
+    ],
+    "assembly": [
+        {
+            "assembly_id": "01",
+            "customer_id": "01",
+            "assembly_number": "cust01.ab01",
+            "revision": "ab01.rev01"
+        },
+        {
+            "assembly_id": "02",
+            "customer_id": "02",
+            "assembly_number": "cust02.ab02",
+            "revision": "ab02.rev01"
+        },
+        {
+            "assembly_id": "03",
+            "customer_id": "03",
+            "assembly_number": "cust03.ab03",
+            "revision": "ab03.rev01"
+        },
+        {
+            "assembly_id": "04",
+            "customer_id": "01",
+            "assembly_number": "cust01.ab04",
+            "revision": "ab04.rev01"
+        },
+        {
+            "assembly_id": "05",
+            "customer_id": "01",
+            "assembly_number": "cust03.ab05",
+            "revision": [
+                {
+                    "01": "ab05.rev01"
+                },
+                {
+                    "02": "ab05.rev01"
+                },
+                {
+                    "03": "ab05.rev01"
+                }
+            ]
+        },
+        {
+            "assembly_id": "06",
+            "customer_id": "04",
+            "assembly_number": "cust04.ab06",
+            "revision": "ab06.rev01"
+        },
+        {
+            "assembly_id": "07",
+            "customer_id": "04",
+            "assembly_number": "cust04.ab06",
+            "revision": "ab06.rev02"
+        },
+        {
+            "assembly_id": "08",
+            "customer_id": "04",
+            "assembly_number": "cust04.ab06",
+            "revision": "ab06.rev03"
+        },
+        {
+            "assembly_id": "09",
+            "customer_id": "05",
+            "assembly_number": "cust05.ab09",
+            "revision": "ab09.rev01"
+        },
+        {
+            "assembly_id": "10",
+            "customer_id": "05",
+            "assembly_number": "cust05.ab10",
+            "revision": "ab10.rev01"
+        },
+        {
+            "assembly_id": "11",
+            "customer_id": "06",
+            "assembly_number": "cust06.ab09",
+            "revision": "ab09.rev01"
+        },
+        {
+            "assembly_id": "12",
+            "customer_id": "06",
+            "assembly_number": "cust06.ab12",
+            "revision": "ab12.rev03"
+        }
+    ],
+    "timeline": [
+        {
+            "assembly_id": "01",
+            "date_received": "20150118-14-23",
+            "date_completed": "20150127-09-12",
+            "pcb_date": "20150120-09-50",
+            "schedule": "20150122-10-20"
+        },
+        {
+            "assembly_id": "02",
+            "date_received": "20150218-14-23",
+            "date_completed": "20150227-09-12",
+            "pcb_date": "20150220-09-50",
+            "schedule": "20150222-10-20"
+        },
+        {
+            "assembly_id": "03",
+            "date_received": "20150318-14-23",
+            "date_completed": "20150327-09-12",
+            "pcb_date": "20150320-09-50",
+            "schedule": "20150322-10-20"
+        },
+        {
+            "assembly_id": "04",
+            "date_received": "20150418-14-23",
+            "date_completed": "20150427-09-12",
+            "pcb_date": "20150420-09-50",
+            "schedule": "20150422-10-20"
+        },
+        {
+            "assembly_id": "05",
+            "date_received": "20150518-14-23",
+            "date_completed": "20150527-09-12",
+            "pcb_date": "20150520-09-50",
+            "schedule": "20150522-10-20"
+        },
+        {
+            "assembly_id": "06",
+            "date_received": "20150618-14-23",
+            "date_completed": "20150627-09-12",
+            "pcb_date": "20150620-09-50",
+            "schedule": "20150622-10-20"
+        },
+        {
+            "assembly_id": "07",
+            "date_received": "20150718-14-23",
+            "date_completed": "20150727-09-12",
+            "pcb_date": "20150720-09-50",
+            "schedule": "20150722-10-20"
+        },
+        {
+            "assembly_id": "08",
+            "date_received": "20150818-14-23",
+            "date_completed": "20150827-09-12",
+            "pcb_date": "20150820-09-50",
+            "schedule": "20150822-10-20"
+        },
+        {
+            "assembly_id": "09",
+            "date_received": "20150918-14-23",
+            "date_completed": "20150927-09-12",
+            "pcb_date": "20150920-09-50",
+            "schedule": "20150922-10-20"
+        },
+        {
+            "assembly_id": "10",
+            "date_received": "20151018-14-23",
+            "date_completed": "20151027-10-12",
+            "pcb_date": "20151020-10-50",
+            "schedule": "20151022-11-20"
+        },
+        {
+            "assembly_id": "11",
+            "date_received": "20151118-14-23",
+            "date_completed": "20151127-11-12",
+            "pcb_date": "20151120-11-50",
+            "schedule": "20151122-11-20"
+        },
+        {
+            "assembly_id": "12",
+            "date_received": "20151218-14-23",
+            "date_completed": "20151227-12-12",
+            "pcb_date": "20151220-12-50",
+            "schedule": "20151222-12-20"
+        }
+    ]
+};
---
+++ plugins/Excelcharts/sample/desc.php
@@ -0,0 +1,4 @@
+<?php
+    $json_file = file_get_contents("all_data.json");
+    print_r($json_file);
+?>
